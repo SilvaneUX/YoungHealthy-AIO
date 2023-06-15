4dad48176cca402acba0df7488244238a6fbb93f
SilvaneUX <46640918+SilvaneUX@users.noreply.github.com>
2023-06-15 20:57:24 +0700

    v0.001
    test anotherapproach url (1)

--- src/scripts/globals/config.js
+++ src/scripts/globals/config.js
@@ -1,7 +1,7 @@
 const CONFIG = {
-  BASE_URL: 'younghealthy-beken-production.up.railway.app/articles-trending',
-  MEDICINE : 'younghealthy-beken-production.up.railway.app/medicine/categories/obat/page/1',
-  REKOMENDASI : 'younghealthy-beken-production.up.railway.app/articles/page/1',
+  BASE_URL: 'https://younghealthy-beken-production.up.railway.app/articles-trending',
+  MEDICINE : 'https://younghealthy-beken-production.up.railway.app/medicine/categories/obat/page/1',
+  REKOMENDASI : 'https://younghealthy-beken-production.up.railway.app/articles/page/1',
   CACHE_NAME: new Date().toISOString(),
   DATABASE_NAME: 'restaurant-catalogue-database2',
   DATABASE_VERSION: 1,
